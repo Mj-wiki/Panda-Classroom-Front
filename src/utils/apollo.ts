cfbe3e7763df173d2ad09c148db9dc7edf871590
Blackstone <601735416@qq.com>
2023-08-11 00:00:00 +0800

    feat: 🎸 查缺补漏解决一些遗留问题

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -1,10 +1,17 @@
 import { currentOrg } from '@/utils';
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+import { message } from 'antd';
+import { onError } from '@apollo/client/link/error'; // 引入onError
 import { AUTH_TOKEN } from './constants';
 
+let uri = `http://${window.location.hostname}:3000/graphql`;
+if (process.env.NODE_ENV === 'production') {
+  uri = 'https://water-drop.yondu.vip/graphql';
+}
+
 const httpLink = createHttpLink({
-  uri: '//localhost:3000/graphql',
+  uri,
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -18,8 +25,25 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const errorLink = onError(({
+  graphQLErrors,
+  networkError,
+}) => {
+  if (graphQLErrors) {
+    message.error('请求参数或者返回的数据格式不对');
+    graphQLErrors.forEach((item) => {
+      if (item.message === 'Unauthorized') {
+        message.error('登录失效，请登录');
+      }
+    });
+  }
+  if (networkError) {
+    message.error(networkError.message);
+  }
+});
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: errorLink.concat(authLink.concat(httpLink)),
   defaultOptions: {
     watchQuery: {
       fetchPolicy: 'no-cache',
